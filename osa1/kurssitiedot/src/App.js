2cebef956f59e8f049883b89a261ff9daaa75b90
Pekka <75335748+P3Xu@users.noreply.github.com>
2022-12-25 19:23:43 +0200

    Assignments 1.1-1.2
    Restarted course after a year of pause

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const Header = (props) => (
   <h1>{props.course}</h1>
 )
@@ -8,53 +6,29 @@ const Part = (props) => (
   <p>{props.part} {props.exercise}</p>
 )
 
-const Content = (props) => {
-  let results = [];
+const Total = (props) => (
+  <p>Number of exercises {props.exercises.reduce((a, b) => a + b, 0)}</p>
+)
 
-  for (let i = 0; i < props.parts.length; i++) {
-    results.push(<Part part={props.parts[i]} exercise={props.exercises[i]} />)
-  }
-
-  return (
-    <>
-      {results[0]}
-      {results[1]}
-      {results[2]}
-    </>
-  )
-}
-
-const Total = (props) => {
-  let sum = 0;
-
-  for(let i = 0; i < props.exercises.length; i++) {
-      sum += props.exercises[i];
-  }
-
-  return (
-      <p>Number of exercises {sum}</p>
-  )
-}
+const Content = (props) => (
+  props.parts.map((part, i) => <Part part={part} exercise={props.exercises[i]} />)
+)
 
 const App = () => {
   const course = 'Half Stack application development'
+  const exercises = [10, 7, 14]
   const parts = [
     'Fundamentals of React',
     'Using props to pass data',
     'State of a component'
   ]
-  const exercises = [
-    10,
-    7,
-    14
-  ]
 
   return (
-    <div>
+    <>
       <Header course={course} />
       <Content parts={parts} exercises={exercises} />
       <Total exercises={exercises} />
-    </div>
+    </>
   )
 }
 
