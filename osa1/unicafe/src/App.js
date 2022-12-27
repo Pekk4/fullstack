aeb062dceb3c9f0a458137923d3875412f8d03c7
Pekka <75335748+P3Xu@users.noreply.github.com>
2022-12-27 15:46:18 +0200

    Assignment 1.9

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -15,28 +15,38 @@ const Display = ({ text, count, end_char }) => (
 )
 
 const Statistics = ({ statistics }) => {
-  const [good, neutral, bad] = statistics
-  const sum = statistics.reduce((a, b) => a + b)
-  const average = (good + (-1 * bad)) / sum
-  const percentage = (good / sum) * 100
+  const sum = Object.values(statistics).reduce((a, b) => a + b)
+  const good = statistics["Good"]
+  const bad = statistics["Bad"]
+  const results = [<Header text="Statistics" />]
 
-  return (
-    <>
-      <Display text="Good" count={good} />
-      <Display text="Neutral" count={neutral} />
-      <Display text="Bad" count={bad} />
-      <Display text="All" count={sum} />
-      <Display text="Average" count={average} />
-      <Display text="Positive" count={percentage} end_char="%" />
-    </>
-  )
+  statistics["Average"] = (good + (-1 * bad)) / sum
+  statistics["Percentage"] = (good / sum) * 100
+
+  if (sum > 0) {
+    for (const [key, value] of Object.entries(statistics)) {
+      if (key === "Percentage") {
+        results.push(<Display text={key} count={value} end_char="%" />)
+      } else {
+        results.push(<Display text={key} count={value} />)
+      }
+    }
+  } else {
+    results.push(<p>No feedback given</p>)
+  }
+
+  return results
 }
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const statistics = [good, neutral, bad]
+  const statistics = {
+    "Good": good,
+    "Neutral": neutral,
+    "Bad": bad
+  }
 
   return (
     <div>
@@ -44,11 +54,9 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text="Good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="Bad" />
-      <Header text="Statistics" />
       <Statistics statistics={statistics} />
     </div>
   )
-
 }
 
 export default App;
