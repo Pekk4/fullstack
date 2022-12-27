d05807db02778b89166c47991883b5219b753450
Pekka <75335748+P3Xu@users.noreply.github.com>
2022-12-27 13:56:46 +0200

    Assignment 1.8

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -10,14 +10,33 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
-const Display = ({text, count}) => (
-  <p>{text}: {count}</p>
+const Display = ({ text, count, end_char }) => (
+  <p>{text}: {count} {end_char}</p>
 )
+
+const Statistics = ({ statistics }) => {
+  const [good, neutral, bad] = statistics
+  const sum = statistics.reduce((a, b) => a + b)
+  const average = (good + (-1 * bad)) / sum
+  const percentage = (good / sum) * 100
+
+  return (
+    <>
+      <Display text="Good" count={good} />
+      <Display text="Neutral" count={neutral} />
+      <Display text="Bad" count={bad} />
+      <Display text="All" count={sum} />
+      <Display text="Average" count={average} />
+      <Display text="Positive" count={percentage} end_char="%" />
+    </>
+  )
+}
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const statistics = [good, neutral, bad]
 
   return (
     <div>
@@ -26,9 +45,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="Bad" />
       <Header text="Statistics" />
-      <Display text="Good" count={good} />
-      <Display text="Neutral" count={neutral} />
-      <Display text="Bad" count={bad} />
+      <Statistics statistics={statistics} />
     </div>
   )
 
