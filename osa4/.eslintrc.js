fe87ce4801b43a3c11c365401c47e8d11614acf4
Pekka <75335748+Pekk4@users.noreply.github.com>
2024-03-10 18:16:38 +0200

    Assignment 4.2

--- osa4/.eslintrc.js
+++ osa4/.eslintrc.js
@@ -5,7 +5,28 @@ module.exports = {
         "es2021": true,
         "node": true
     },
+    "plugins": [
+        "@stylistic/js",
+    ],
     "extends": "eslint:recommended",
+    'rules': {
+        '@stylistic/js/indent': [
+            'error',
+            2
+        ],
+        '@stylistic/js/linebreak-style': [
+            'error',
+            'unix'
+        ],
+        '@stylistic/js/quotes': [
+            'error',
+            'single'
+        ],
+        '@stylistic/js/semi': [
+            'error',
+            'never'
+        ],
+    },
     "overrides": [
         {
             "env": {
@@ -38,6 +59,15 @@ module.exports = {
         "semi": [
             "error",
             "never"
-        ]
+        ],
+        'eqeqeq': 'error',
+        'no-trailing-spaces': 'error',
+        'object-curly-spacing': [
+            'error', 'always'
+        ],
+        'arrow-spacing': [
+            'error', { 'before': true, 'after': true }
+        ],
+        'no-console': 'error'
     }
 }
