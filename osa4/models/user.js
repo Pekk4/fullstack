e1b5ad27e96d80db4f07c0493b7d3e00eda975f8
Pekka <75335748+Pekk4@users.noreply.github.com>
2024-03-24 17:16:23 +0200

    Assignment 4.15

---
+++ osa4/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: String,
+  passwordHash: String,
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Blog'
+    }
+  ],
+})
+
+module.exports = mongoose.model('User', userSchema)
