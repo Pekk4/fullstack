1527f74a8ceb6b16f5a68e9da00f2042399be6e7
Pekka <75335748+Pekk4@users.noreply.github.com>
2024-04-08 20:34:12 +0300

    Assignment 5.7

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,46 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [detailedView, setDetailedView] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleView = () => {
+    setDetailedView(!detailedView)
+  }
+
+  if (!detailedView) {
+    return (
+      <div style={blogStyle}>
+        <p>
+          {blog.title}{' '}
+          {blog.author}{' '}
+          <button onClick={toggleView}>View</button>
+        </p>
+      </div>
+    )
+  } else {
+    return (
+      <div style={blogStyle}>
+        <p>
+          {blog.title}{' '}
+          {blog.author}{' '}
+          <button onClick={toggleView}>Cancel</button>
+          <br />
+          {blog.url}
+          <br />
+          Likes {blog.likes} <button>Like</button>
+          <br />
+          {blog.user.name}
+        </p>
+      </div>
+    )
+  }
+}
 
 export default Blog
