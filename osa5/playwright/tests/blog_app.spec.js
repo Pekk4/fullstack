7b7da756211066d52703e07d3d6ef7c239f7bf30
Pekka <75335748+Pekk4@users.noreply.github.com>
2024-04-13 19:38:43 +0300

    Assignment 5.17

---
+++ osa5/playwright/tests/blog_app.spec.js
@@ -0,0 +1,21 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'Matti Luukkainen',
+        username: 'mluukkai',
+        password: 'salainen'
+      }
+    })
+
+    await page.goto('/')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByText('Username')).toBeVisible()
+    await expect(page.getByText('Password')).toBeVisible()
+  })
+})
