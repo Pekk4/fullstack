9e6a6d7771597d952626722bb0e86f979506952e
Pekka <75335748+Pekk4@users.noreply.github.com>
2024-06-09 21:54:55 +0300

    Assignment 6.9

--- osa6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ osa6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -3,7 +3,19 @@ import { useDispatch, useSelector } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => {
+    const anecdotes = state.anecdotes
+    const filter = state.filter
+
+    if ( filter === 'ALL' ) {
+      return anecdotes
+    }
+
+    return anecdotes.filter(a => a
+      .content
+      .toLowerCase()
+      .includes(filter.toLowerCase()))
+  })
   const dispatch = useDispatch()
 
   const vote = (id) => {
