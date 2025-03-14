aee2ea43f4146d428bdf35b8b3fece5d2fe0be1e
Peksu <75335748+Pekk4@users.noreply.github.com>
2025-03-14 19:55:29 +0200

    Assignment 9.9

--- osa9/patientor/backend/index.ts
+++ osa9/patientor/backend/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
