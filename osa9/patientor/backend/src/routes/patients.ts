7cb059417aa1922bc2f8c9d7f1a27a0138949a81
Peksu <75335748+Pekk4@users.noreply.github.com>
2025-03-31 21:51:40 +0300

    Assignment 9.13

--- osa9/patientor/backend/src/routes/patients.ts
+++ osa9/patientor/backend/src/routes/patients.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import patientService from '../services/patientService';
+import toNewPatient  from '../utils';
 
 const router = express.Router();
 
@@ -9,17 +10,18 @@ router.get('/', (_req, res) => {
 });
 
 router.post('/', (req, res) => {
-  /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-  const {name, dateOfBirth, ssn, gender, occupation } = req.body;
-  const newPatient = {
-    id: Math.random().toString(36).substring(2, 15),
-    name,
-    dateOfBirth,
-    ssn,
-    gender,
-    occupation
-  };
-  res.json(newPatient);
+  try {
+    const newPatient = toNewPatient(req.body);
+    const addedPatient = patientService.addPatient(newPatient);
+    console.log(addedPatient);
+    res.json(addedPatient);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong';
+    if (error instanceof Error) {
+      errorMessage += ` Error: ${error.message}`;
+    }
+    res.status(400).send(errorMessage);
+  }
 });
 
 export default router;
