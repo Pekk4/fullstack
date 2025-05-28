79ad2317ca6f96b41183c895db394a8a08b6a6b4
Pekka <75335748+Pekk4@users.noreply.github.com>
2025-05-28 13:16:49 +0300

    Assignment 9.23

--- osa9/patientor/backend/src/types.ts
+++ osa9/patientor/backend/src/types.ts
@@ -14,10 +14,6 @@ export enum Gender {
   Other = "other"
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {
-}
-
 export interface Patient {
   id: string;
   name: string;
@@ -25,9 +21,51 @@ export interface Patient {
   ssn: string;
   gender: Gender;
   occupation: string;
-  entries?: Entry[];
+  entries: Entry[];
 };
 
 export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
 
 export type NewPatient = z.infer<typeof newPatientSchema>;
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
