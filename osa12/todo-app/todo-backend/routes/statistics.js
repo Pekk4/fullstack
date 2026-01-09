2b1eb247d418ccab357d0ada79bbe9a385987a9b
Pekka <75335748+Pekk4@users.noreply.github.com>
2026-01-09 20:03:24 +0200

    Assignment 12.10

---
+++ osa12/todo-app/todo-backend/routes/statistics.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const { getAsync } = require('../redis')
+
+router.get('/', async (_, res) => {
+  const added_todos = await getAsync('added_todos') || 0
+
+  res.send({
+    added_todos: parseInt(added_todos)
+  });
+});
+
+module.exports = router;
