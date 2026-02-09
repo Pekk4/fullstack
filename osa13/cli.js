60f09a202cd4ceb38010e226ab31588aaa31ae06
Pekka <75335748+Pekk4@users.noreply.github.com>
2026-02-09 22:11:09 +0200

    Assignment 13.4

--- osa13/cli.js
+++ osa13/cli.js
@@ -4,6 +4,10 @@ const {
   Model,
   DataTypes
 } = require('sequelize');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
 
 const sequelize = new Sequelize(process.env.DATABASE_URL);
 
@@ -40,10 +44,35 @@ Blog.init(
   }
 );
 
-Blog.findAll().then((blogs) =>
-  blogs.forEach((blog) =>
-    console.log(
-      `${blog.dataValues.author}: '${blog.dataValues.title}', ${blog.dataValues.likes} likes`
-    )
-  )
-);
+Blog.sync();
+
+app.get('/api/blogs', async (_, res) => {
+  const blogs = await Blog.findAll();
+
+  res.json(blogs);
+});
+
+app.post('/api/blogs', async (req, res) => {
+  try {
+    const newBlog = await Blog.create(req.body);
+
+    res.json(newBlog);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  const id = req.params.id;
+  const blogToDelete = await Blog.findByPk(id);
+
+  await blogToDelete.destroy();
+
+  res.json({ message: 'Blog deleted' });
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
