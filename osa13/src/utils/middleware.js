2ad163de234787abe56dc4e665b5e20530110aa8
Pekka <75335748+Pekk4@users.noreply.github.com>
2026-02-25 21:54:33 +0200

    Assignment 13.9

--- osa13/src/utils/middleware.js
+++ osa13/src/utils/middleware.js
@@ -12,9 +12,22 @@ const blogFinder = async (req, res, next) => {
 
 const errorHandler = (error, _, res, next) => {
   if (error.name === 'SequelizeValidationError') {
-    return res.status(400).json({ error: error.message });
+    const emailError = error.errors.find((e) => e.path === 'username');
+
+    if (emailError?.validatorKey === 'isEmail') {
+      //console.log(emailError.message);
+      return res.status(400).json({ error: "username must be a valid email address" });
+    } else {
+      return res.status(400).json({ error: error.message });
+    }
   } else if (error.name === 'SequelizeUniqueConstraintError') {
-    return res.status(400).json({ error: error.message });
+    const emailError = error.errors.find((e) => e.path === 'username');
+
+    if (emailError) {
+      return res.status(400).json({ error: "username must be a valid email address" });
+    } else {
+      return res.status(400).json({ error: error.message });
+    }
   } else if (error.name === 'SequelizeDatabaseError') {
     return res.status(400).json({ error: error.message });
   }
