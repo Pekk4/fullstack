8218f5c2dc79d569fb8ae239b2f2ab6e3cbda5b6
Pekka <75335748+Pekk4@users.noreply.github.com>
2026-02-28 13:11:46 +0200

    Assignment 13.16

---
+++ osa13/src/controllers/authors.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+
+const { sequelize } = require('../utils/db');
+const { Blog } = require('../models');
+
+router.get('/', async (_, res) => {
+  const authors = await Blog.findAll({
+    attributes: [
+      'author',
+      [sequelize.fn('COUNT', sequelize.col('*')), 'articles'],
+      [sequelize.fn('SUM', sequelize.col('likes')), 'likes'],
+    ],
+    group: 'author',
+    order: [['likes', 'DESC']],
+  });
+
+  res.json(authors);
+});
+
+module.exports = router;
