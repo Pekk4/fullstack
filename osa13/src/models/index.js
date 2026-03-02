de0712fe64afee856c963aca5ae92ebf545d93f3
Pekka <75335748+Pekk4@users.noreply.github.com>
2026-03-02 18:45:39 +0200

    Assignment 13.20

--- osa13/src/models/index.js
+++ osa13/src/models/index.js
@@ -1,10 +1,15 @@
 const Blog = require('./blog');
 const User = require('./user');
+const List = require('./list');
 
 User.hasMany(Blog);
 Blog.belongsTo(User);
 
+Blog.belongsToMany(User, { through: 'lists' });
+User.belongsToMany(Blog, { through: 'lists' });
+
 module.exports = {
   Blog,
   User,
+  List,
 };
