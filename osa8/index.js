e95016bf2a3de7fa8d298c1ff3249cc5a0736e33
Pekka <75335748+Pekk4@users.noreply.github.com>
2026-03-07 20:18:04 +0200

    Assignment 8.7

--- osa8/index.js
+++ osa8/index.js
@@ -124,6 +124,10 @@ const typeDefs = /* GraphQL */ `
       author: String!
       genres: [String!]!
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -161,6 +165,18 @@ const resolvers = {
       books = books.concat(book)
 
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)
+
+      if (!author) {
+        return null
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+
+      return updatedAuthor
     }
   }
 }
