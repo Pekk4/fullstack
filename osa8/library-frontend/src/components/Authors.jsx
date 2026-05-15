557ab7ca37cad082ee36e1e3a916f58027213b54
Pekka <75335748+Pekk4@users.noreply.github.com>
2026-05-15 16:02:05 +0300

    Assignment 8.12

--- osa8/library-frontend/src/components/Authors.jsx
+++ osa8/library-frontend/src/components/Authors.jsx
@@ -51,10 +51,14 @@ const Authors = (props) => {
         <form onSubmit={submit}>
           <div>
             name
-            <input
-              value={name}
-              onChange={({ target }) => setName(target.value)}
-            />
+            <select value={name} onChange={({ target }) => setName(target.value)}>
+              <option value="">select author</option>
+              {authors.map((a) => (
+                <option key={a.id} value={a.name}>
+                  {a.name}
+                </option>
+              ))}
+            </select>
           </div>
           <div>
             born
