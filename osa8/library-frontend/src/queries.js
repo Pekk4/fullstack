d2c6d2d559832f6dd71750e6f061581f2ae9f00e
Pekka <75335748+Pekk4@users.noreply.github.com>
2026-05-25 11:35:03 +0300

    Assignment 8.18

--- osa8/library-frontend/src/queries.js
+++ osa8/library-frontend/src/queries.js
@@ -13,9 +13,12 @@ export const ALL_AUTHORS = gql`
 export const ALL_BOOKS = gql`
   query {
     allBooks {
+      id
       title
       published
-      author
+      author {
+        name
+      }
     }
   }
 `
